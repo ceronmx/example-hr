e8ca974d27d1881f51478992ea86b6967e3de505
Daniel Pérez <danielzala.v2@gmail.com>
2026-04-21 00:14:19 -0600

    feat: implement hexagonal architecture and dependency boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import boundaries from 'eslint-plugin-boundaries';
+import { fixupPluginRules } from '@eslint/compat';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -12,6 +14,33 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      boundaries: fixupPluginRules(boundaries),
+    },
+    settings: {
+      'boundaries/elements': [
+        {
+          type: 'domain',
+          pattern: 'src/domain/**',
+          mode: 'full',
+        },
+        {
+          type: 'application',
+          pattern: 'src/application/**',
+          mode: 'full',
+        },
+        {
+          type: 'infrastructure',
+          pattern: 'src/infrastructure/**',
+          mode: 'full',
+        },
+        {
+          type: 'main',
+          pattern: 'src/main.ts',
+          mode: 'full',
+        },
+      ],
+    },
     languageOptions: {
       globals: {
         ...globals.node,
@@ -23,13 +52,35 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'boundaries/dependencies': [
+        'error',
+        {
+          default: 'disallow',
+          rules: [
+            {
+              from: 'domain',
+              allow: ['domain'],
+            },
+            {
+              from: 'application',
+              allow: ['domain', 'application'],
+            },
+            {
+              from: 'infrastructure',
+              allow: ['domain', 'application', 'infrastructure'],
+            },
+            {
+              from: 'main',
+              allow: ['infrastructure'],
+            },
+          ],
+        },
+      ],
     },
   },
 );
